be8a14717b58cd2adbcc9725b8fe1aec9945f6bf
beerfect <beerfect@yandex.ru>
2019-04-30 23:13:23 +0300

    button edition

--- DrumShowLED.ino
+++ DrumShowLED.ino
@@ -1,15 +1,14 @@
-#include <FastLED.h>                                    //подключаем библиотеку
-#define NUM_LEDS 60                                     // общее количество светодиодов
-#define DATA_PIN 5                                      // пин подключенный к ленте
-//#define BRIGHTNESS 0
-CRGB leds[NUM_LEDS];                                    // массив для манипуляции светодиодами
+#include <FastLED.h>    //подключаем библиотеку
+#define NUM_LEDS 60     // общее количество светодиодов
+#define DATA_PIN 5      // пин подключенный к ленте
+CRGB leds[NUM_LEDS];    // массив для манипуляции светодиодами
 
-float brigtness = 0;
-float dimStep = 5; //0.5, 1, 3, 5, 15, 17, 42.5
+float brigtness = 0;    // дефолтная яркость
+float blackoutStep = 5; //доступные шаги затемнения: 0.5, 1, 3, 5, 15, 17, 42.5 
 
-int color1 = 0;
-int color2 = 96;
-int color3 = 160;
+int color1 = 20;         // цвет ленты 1
+int color2 = 120;        // цвет ленты 2
+int color3 = 190;       // цвет ленте 3
 
 const int buttonPin = 2; // Куда подключена кнопка
 
@@ -20,43 +19,29 @@ void setup() {
 
 void loop() {
 
+  // по умолчанию все диоды имеют яркость = 0 (полностью темные)
   fill_solid( &(leds[0]), NUM_LEDS / 3, CHSV( color1, 255, brigtness ) );
   fill_solid( &(leds[NUM_LEDS / 3 ]), NUM_LEDS / 3, CHSV( color2, 255, brigtness));
   fill_solid( &(leds[NUM_LEDS - NUM_LEDS / 3]), NUM_LEDS / 3, CHSV( color3, 255, brigtness) );
 
-  //  FastLED.show();
-
+  // по нажатию кнопки устанавливаем яркость на максимум
   if (digitalRead(buttonPin)) {
     brigtness = 255;
     FastLED.show();
     while (digitalRead(buttonPin)) {}
   }
+
+  // постепенно затемняем
   while (brigtness > 0) {
-    brigtness -= dimStep;
+    brigtness -= blackoutStep;
     fill_solid( &(leds[0]), NUM_LEDS / 3, CHSV( color1, 255, brigtness) );
     fill_solid( &(leds[NUM_LEDS / 3 ]), NUM_LEDS / 3, CHSV( color2, 255, brigtness));
     fill_solid( &(leds[NUM_LEDS - NUM_LEDS / 3]), NUM_LEDS / 3, CHSV( color3, 255, brigtness) );
-
     FastLED.show();
+
+    // если нажали во время затемнения - выход из цикла 
     if (digitalRead(buttonPin)) {
       break;
     }
   }
-
-
-
 }
-
-
-
-
-
-
-//  if (digitalRead(buttonPin)) {
-//    for (int i = 256; i >= 0 ; i -= 1) {
-//      fill_solid( &(leds[0]), NUM_LEDS / 3, CHSV( 0, 255, i) );
-//      fill_solid( &(leds[NUM_LEDS / 3 ]), NUM_LEDS / 3, CHSV( 96, 255, i) );
-//      fill_solid( &(leds[NUM_LEDS - NUM_LEDS / 3]), NUM_LEDS / 3, CHSV( 160, 255, i) );
-//      FastLED.show();
-//    }
-//  }
