406707dfea878f314519720d92d8ca75f0e5ba2c
beerfect <beerfect@yandex.ru>
2019-07-04 18:24:23 +0300

    Update DrumShowLED.ino
     better interface, better naming, comments added

--- DrumShowLED.ino
+++ DrumShowLED.ino
@@ -1,32 +1,32 @@
-// Подключаем библиотеку
-#include <FastLED.h>
-#define NUM_LEDS 210
-#define DATA_PIN 8
-CRGB leds[NUM_LEDS];
+#include <FastLED.h>    // Подключаем библиотеку для управления LED лентой
+#define NUM_LEDS 210    // Общее число светодиодов
+#define DATA_PIN 8      // Пин подключения LED ленты
+CRGB leds[NUM_LEDS];    // Массив для управления LED лентой
 
 // Подключаем устройства
-const uint8_t pinTouchSensor[3] = {3, 4, 5};
-const uint8_t pinSoundSensor[3] = {A4, A5, A6};
-const uint8_t pinColorSlider[3] = {A0, A1, A2};
-const uint8_t pinFadeSlider = A3;
-const uint8_t pinModeChangeButton = 2;
+const uint8_t pinTouchSensor[3] = {3, 4, 5};    // Пины подключения сенсорных кнопок
+const uint8_t pinSoundSensor[3] = {A4, A5, A6}; // Пины подключения датчиков звука
+const uint8_t pinColorSlider[3] = {A0, A1, A2}; // Пины подключения слйдеров изменения HUE
+const uint8_t pinFadeSlider = A3;               // Пин подключения слайдера затемнения
+const uint8_t pinModeChangeButton = 2;          // Пин подключения кнопки пепеключения режимов
 
 // Разметка ленты
-const uint8_t partLength = 60;
-const uint8_t startPart[3] = {30, 90, 150};
+const uint8_t partLength = 60;              // Количество светодиодов у каждого барабана
+const uint8_t startPart[3] = {30, 90, 150}; // Начала отрезков ленты
 
 // Переменные
-uint8_t currentMode = 0;
-uint8_t colorSliderValue[3];
-uint8_t hue[3];
-uint8_t saturation[3];
-float value[3];
-float fadeStep;
+uint8_t currentMode = 0;        // Текущий режим
+uint8_t currentHue[3];          // Текущие Hue
+uint8_t neededHue[3];           // Hue к которым нужно придти
+uint8_t ledsSaturation[3];      // Saturation's для лент
+uint8_t interfaceSaturation[3]; // Saturation's для интерфейса
+float value[3];                 // Value's
+float fadeStep;                 // Скорость затемнения после удара
 
 void setup() {
-  Serial.begin(9600);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  leds[4] = 0x001100; leds[5] = 0x111111; leds[6] = 0x001100;
+  Serial.begin(9600);                                         // Подключаем монитор порта для отладки
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);        // Доподключаем библиотеку FastLED
+  leds[4] = 0x001100; leds[5] = 0x111111; leds[6] = 0x001100; // Отображаем в интерфейсе режим по умолчанию
 }
 
 void loop() {
@@ -38,9 +38,9 @@ void loop() {
 
   // Вычисляем и плавно изменяем HUE и SATURATION на основании показаний слайдера цвета
   for (uint8_t i = 0; i < 3; i++) {
-    colorSliderValue[i] = transformSliderValueToColor(analogRead(pinColorSlider[i]));
-    hue[i] = smootlyChangeCurrentColorToSettedOnSlider(hue[i], colorSliderValue[i]);
-    saturation[i] = smoothlyRemoveSaturationAtTheTopPositionOfTheColorSlider(saturation[i], hue[i]);
+    neededHue[i] = getHue(analogRead(pinColorSlider[i]));
+    currentHue[i] = smoothlyChangeHue(currentHue[i], neededHue[i]);
+    ledsSaturation[i] = smoothlyChangeSaturation(ledsSaturation[i], currentHue[i]);
   }
 
   // Слушаем датчики, применяем режимы
@@ -69,7 +69,7 @@ void loop() {
 
   // Подсвечиваем ленты вычисленными HSV
   for (uint8_t i = 0; i < 3; i++) {
-    fill_solid(&(leds[startPart[i]]), partLength, CHSV(hue[i], saturation[i], value[i]));
+    fill_solid(&(leds[startPart[i]]), partLength, CHSV(currentHue[i], ledsSaturation[i], value[i]));
   }  
   FastLED.show();
   
@@ -84,13 +84,15 @@ void loop() {
 
   // Интерфейс
   for (uint8_t i = 0; i < 3; i++) {
-    fill_solid(&(leds[i]), 1, CHSV(hue[i], saturation[i], 255 ));
-  }
-
-  
+    if(analogRead(pinColorSlider[i]) > 1021) interfaceSaturation[i] = 0;
+    else interfaceSaturation[i] = 255;
+    fill_solid(&(leds[i]), 1, CHSV(neededHue[i], interfaceSaturation[i], 255 ));
+  }  
 }
 
 // ФУНКЦИИ
+
+// Циклично переключает режимы (0/1/2), отображает индикацию текущего режима 
 void changeMode() {
   switch (currentMode) {
     case 0:
@@ -114,28 +116,29 @@ void changeMode() {
   }
 }
 
-uint8_t transformSliderValueToColor(int sliderValue) {
-  uint8_t color;
-  if      (sliderValue == 0)    color = 0;
-  else if (sliderValue < 15)    color = 32;
-  else if (sliderValue < 92)    color = 64;
-  else if (sliderValue < 409)   color = 96;
-  else if (sliderValue < 725)   color = 128;
-  else if (sliderValue < 981)   color = 160;
-  else if (sliderValue < 1011)  color = 192;
-  else if (sliderValue < 1022)  color = 244;
-  else                          color = 254;
-  return color;
+// Диапазон хода слайдера разбит на равные отрезки, за каждым отрезком закреплен свой цвет
+uint8_t getHue(int sliderValue) {
+   if      (sliderValue == 0)    return 0;    // Красный (нижнее положение слайдера)
+  else if (sliderValue < 15)    return 32;   // Оранжевый
+  else if (sliderValue < 92)    return 64;   // Желтый
+  else if (sliderValue < 409)   return 96;   // Зеленый
+  else if (sliderValue < 725)   return 128;  // Голубой
+  else if (sliderValue < 981)   return 160;  // Синий
+  else if (sliderValue < 1011)  return 192;  // Фиолетовый
+  else if (sliderValue < 1022)  return 244;  // Розовый
+  else                          return 254;  // Красный (верхнее положение слайдера)
 }
 
-uint8_t smootlyChangeCurrentColorToSettedOnSlider(uint8_t currentColor, uint8_t neededColor) {
-  uint8_t colorChangeStep = 2;
+// Плавно изменяем текущий цвет на цвет, установленый на слайдере
+uint8_t smoothlyChangeHue(uint8_t currentColor, uint8_t neededColor) {
+  uint8_t colorChangeStep = 1;
   if (currentColor > neededColor) currentColor -= colorChangeStep;
   else if (currentColor < neededColor) currentColor += colorChangeStep;
   return currentColor;
 }
 
-uint8_t smoothlyRemoveSaturationAtTheTopPositionOfTheColorSlider(uint8_t saturation, uint8_t hue) {
+// В верхнем положении слайдера цвета плавно включаем белый цвет
+uint8_t smoothlyChangeSaturation(uint8_t saturation, uint8_t hue) {
   uint8_t saturationChangeStep = 5;
   if      (hue == 254 && saturation > 0)                      saturation -= saturationChangeStep;
   else if (hue == 254 && saturation <= saturationChangeStep)  saturation = 0;
@@ -143,6 +146,7 @@ uint8_t smoothlyRemoveSaturationAtTheTopPositionOfTheColorSlider(uint8_t saturat
   return saturation;
 }
 
+// Регулируем слайдером скорость затемнения после удара, отображаем индикацию
 float setFadeStepFromFadeSliderValue(int sliderValue) {
   float fadeStep;
   uint8_t indicator;
