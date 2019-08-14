894dd088d111daf019a4635b03265ccc635eb148
beerfect <beerfect@yandex.ru>
2019-08-14 22:29:11 +0300

    Holiday commit

--- DrumShowLED.ino
+++ DrumShowLED.ino
@@ -3,25 +3,31 @@
 #define DATA_PIN 8      // Пин подключения LED ленты
 CRGB leds[NUM_LEDS];    // Массив для управления LED лентой
 
+const uint8_t qtyDrums = 3;
+
 // Подключаем устройства
-const uint8_t pinTouchSensor[3] = {3, 4, 5};    // Пины подключения сенсорных кнопок
-const uint8_t pinSoundSensor[3] = {A4, A5, A6}; // Пины подключения датчиков звука
-const uint8_t pinColorSlider[3] = {A0, A1, A2}; // Пины подключения слйдеров изменения HUE
-const uint8_t pinFadeSlider = A3;               // Пин подключения слайдера затемнения
-const uint8_t pinModeChangeButton = 2;          // Пин подключения кнопки пепеключения режимов
+const uint8_t pinOnOffButton[qtyDrums] = {3, 4, 5};    // Пины подключения сенсорных кнопок
+const uint8_t pinSoundSensor[qtyDrums] = {A4, A5, A6}; // Пины подключения датчиков звука
+
+const uint8_t pinColorSlider[qtyDrums] = {A0, A1, A2}; // Пины подключения слйдеров изменения Цвет
+const uint8_t pinFadeSlider = A3;                      // Пин подключения слайдера затемнения
+const uint8_t pinModeChangeButton = 2;                 // Пин подключения кнопки пепеключения режимов
 
 // Разметка ленты
-const uint8_t partLength = 60;              // Количество светодиодов у каждого барабана
-const uint8_t startPart[3] = {30, 90, 150}; // Начала отрезков ленты
+const uint8_t partLength = 60;                         // Количество светодиодов у каждого барабана
+const uint8_t startPart[qtyDrums] = {30, 90, 150};     // Начала отрезков ленты
 
 // Переменные
-uint8_t currentMode = 0;        // Текущий режим
-uint8_t currentHue[3];          // Текущие Hue
-uint8_t neededHue[3];           // Hue к которым нужно придти
-uint8_t ledsSaturation[3];      // Saturation's для лент
-uint8_t interfaceSaturation[3]; // Saturation's для интерфейса
-float value[3];                 // Value's
-float fadeStep;                 // Скорость затемнения после удара
+uint8_t currentMode = 0;                             // Текущий режим
+uint8_t currentColor[qtyDrums];                      // Текущие Цвет
+uint8_t neededColor[qtyDrums];                       // Цвет к которому нужно придти
+uint8_t ledsSaturation[qtyDrums];                    // Saturation's для лент
+uint8_t interfaceSaturation[qtyDrums];               // Saturation's для интерфейса
+uint8_t interfaceValue[qtyDrums] = {255, 255, 255};
+float value[qtyDrums];                               // Value's
+float fadeStep;                                      // Скорость затемнения после удара
+uint8_t sensorThreshold[qtyDrums] = {100, 100, 100}; // порог срабатыавния пьезодатчиков
+bool drumIsActive[3] = {true, true, true};           // Вкл\Выкл сенсоров
 
 void setup() {
   Serial.begin(9600);                                         // Подключаем монитор порта для отладки
@@ -30,46 +36,77 @@ void setup() {
 }
 
 void loop() {
+
+  // On/Off датчички
+  
+   for (uint8_t i = 0; i < qtyDrums; i++) {
+    if (digitalRead(pinOnOffButton[i])){
+      drumIsActive[i] = !drumIsActive[i];
+      changeInterface(i);
+    }
+    while(digitalRead(pinOnOffButton[i])){
+    
+    }
+   }
+
+  
+  
   // Переключатель режимов
   if (digitalRead(pinModeChangeButton)) {
     changeMode();
     while (digitalRead(pinModeChangeButton)) {}
   }
 
-  // Вычисляем и плавно изменяем HUE и SATURATION на основании показаний слайдера цвета
-  for (uint8_t i = 0; i < 3; i++) {
-    neededHue[i] = getHue(analogRead(pinColorSlider[i]));
-    currentHue[i] = smoothlyChangeHue(currentHue[i], neededHue[i]);
-    ledsSaturation[i] = smoothlyChangeSaturation(ledsSaturation[i], currentHue[i]);
+  // Вычисляем и плавно изменяем Цвет и SATURATION на основании показаний слайдера цвета
+  for (uint8_t i = 0; i < qtyDrums; i++) {
+    neededColor[i] = getHue(analogRead(pinColorSlider[i]));
+    currentColor[i] = smoothlyChangeColor(currentColor[i], neededColor[i]);
+    ledsSaturation[i] = smoothlyChangeSaturation(ledsSaturation[i], currentColor[i]);
   }
 
   // Слушаем датчики, применяем режимы
-  if (digitalRead(pinTouchSensor[0])) {
-    if (currentMode == 1) {
-      value[0] = 255;
-      value[2] = 255;
+  if (currentMode == 0){
+    if (drumWasKicked(0)){
+      doNothing();
     }
-    else if (currentMode == 2) {
-      value[0] = 255;
+    if (drumWasKicked(1)){
+      if (drumIsActive[0]) value[0] = 255;
+      if (drumIsActive[1]) value[1] = 255;
+      if (drumIsActive[2]) value[2] = 255;
+    }
+    if (drumWasKicked(2)){
+      doNothing();
     }
   }
-
-  if (digitalRead(pinTouchSensor[1])) {
-    if (currentMode == 0) {
-      value[0] = 255;
-      value[1] = 255;
-      value[2] = 255;
+  
+  if (currentMode == 1){
+    if (drumWasKicked(0)){
+      if (drumIsActive[0]) value[0] = 255;
+      if (drumIsActive[2]) value[2] = 255;
     }
-    else value[1] = 255;
+    if (drumWasKicked(1)){      
+      if (drumIsActive[1]) value[1] = 255;      
+    }
+    if (drumWasKicked(2)){
+      doNothing();
+    }
   }
-
-  if (digitalRead(pinTouchSensor[2]) && currentMode == 2) {
-    value[2] = 255;
+  
+  if (currentMode == 2){
+    if (drumWasKicked(0)){
+      if (drumIsActive[0]) value[0] = 255;
+    }
+    if (drumWasKicked(1)){      
+      if (drumIsActive[1]) value[1] = 255;      
+    }
+    if (drumWasKicked(2)){
+      if (drumIsActive[2]) value[2] = 255;
+    }
   }
 
   // Подсвечиваем ленты вычисленными HSV
-  for (uint8_t i = 0; i < 3; i++) {
-    fill_solid(&(leds[startPart[i]]), partLength, CHSV(currentHue[i], ledsSaturation[i], value[i]));
+  for (uint8_t i = 0; i < qtyDrums; i++) {
+    fill_solid(&(leds[startPart[i]]), partLength, CHSV(currentColor[i], ledsSaturation[i], value[i]));
   }  
   FastLED.show();
   
@@ -77,20 +114,36 @@ void loop() {
   fadeStep = setFadeStepFromFadeSliderValue(analogRead(pinFadeSlider));
 
   // Затемняем
-  for (uint8_t i = 0; i < 3; i++) {
+  for (uint8_t i = 0; i < qtyDrums; i++) {
     value[i] -= fadeStep;
     if (value[i] == 0 || value[i] < fadeStep) value[i] = 0;
   }
 
   // Интерфейс
-  for (uint8_t i = 0; i < 3; i++) {
+  for (uint8_t i = 0; i < qtyDrums; i++) {
     if(analogRead(pinColorSlider[i]) > 1021) interfaceSaturation[i] = 0;
     else interfaceSaturation[i] = 255;
-    fill_solid(&(leds[i]), 1, CHSV(neededHue[i], interfaceSaturation[i], 255 ));
+    fill_solid(&(leds[i]), 1, CHSV(neededColor[i], interfaceSaturation[i], interfaceValue[i] ));
   }  
 }
 
+
+
+
+
+
+
 // ФУНКЦИИ
+
+void doNothing(){};
+
+void changeInterface(uint8_t i){
+  if(interfaceValue[i] == 255) {
+    interfaceValue[i] = 60;  
+  } else {
+    interfaceValue[i] = 255; 
+  }
+}
 
 // Циклично переключает режимы (0/1/2), отображает индикацию текущего режима 
 void changeMode() {
@@ -118,7 +171,7 @@ void changeMode() {
 
 // Диапазон хода слайдера разбит на равные отрезки, за каждым отрезком закреплен свой цвет
 uint8_t getHue(int sliderValue) {
-   if      (sliderValue == 0)    return 0;    // Красный (нижнее положение слайдера)
+   if      (sliderValue == 0)   return 0;    // Красный (нижнее положение слайдера)
   else if (sliderValue < 15)    return 32;   // Оранжевый
   else if (sliderValue < 92)    return 64;   // Желтый
   else if (sliderValue < 409)   return 96;   // Зеленый
@@ -130,8 +183,8 @@ uint8_t getHue(int sliderValue) {
 }
 
 // Плавно изменяем текущий цвет на цвет, установленый на слайдере
-uint8_t smoothlyChangeHue(uint8_t currentColor, uint8_t neededColor) {
-  uint8_t colorChangeStep = 1;
+uint8_t smoothlyChangeColor(uint8_t currentColor, uint8_t neededColor) {
+  uint8_t colorChangeStep = 2;
   if (currentColor > neededColor) currentColor -= colorChangeStep;
   else if (currentColor < neededColor) currentColor += colorChangeStep;
   return currentColor;
@@ -163,6 +216,11 @@ float setFadeStepFromFadeSliderValue(int sliderValue) {
   fill_solid(&(leds[8 + indicator]), (6 - indicator), CHSV(0, 255, 100 ));
   fill_solid(&(leds[8]), indicator, CHSV(100, 0, 100 ));
 
-  Serial.println(indicator);
+  //  Serial.println(indicator);
   return fadeStep;
+}
+
+bool drumWasKicked(uint8_t i){
+  bool sensorHeardSound = (analogRead(pinSoundSensor[i]) > sensorThreshold[i]);
+  return sensorHeardSound;
 }
